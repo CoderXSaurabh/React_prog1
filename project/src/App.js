db2cf66abdff80aad4198dda9256f93a36798875
CoderXSaurabh <patilsaurabh5122001@gmail.com>
2024-05-10 11:33:43 +0530

    done with basic page formation

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Routes,Route,Navigate} from 'react-router-dom'
+import NavBar from "./components/NavBar/NavBar";
+
+import ContactList from "./components/Contacts/ContactList/ContactList"
+import AddContacts from "./components/Contacts/AddContacts/AddContact"
+
+import ViewContact from "./components/Contacts/ViewContact/ViewContact"
+
+import EditContact from "./components/Contacts/EditContact/EditContact"
 
 let App = ()=> {
   return (
     <React.Fragment>
-      
-      Hii
+      <NavBar/>
+      <Routes>
+        <Route path={'/'} element={<Navigate to ={'/contacts/list'}/>}/>
+        <Route path={'/contacts/list'} element={<ContactList/>}/>
+        <Route path={'/contacts/add'} element={<AddContacts/>}/>
+        <Route path={'/contacts/view/:contactId'} element={<ViewContact/>}/>
+        <Route path={'/contacts/edit/:contactId'} element={<EditContact/>}/>
+      </Routes>
      
       
     </React.Fragment>
